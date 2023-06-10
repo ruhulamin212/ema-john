9fad013bb85e7854c9fbaf825d7a58aac9f3ceba
ruhulamin212 <ruhulfreelancing@gmail.com>
2023-06-10 23:09:59 +0600

    localStroge

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,6 +4,8 @@ import "./Shop.css";
 import Product from "../Product/Product";
 import "../Cart/Cart";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
+
 
 const Shop = () => {
   // State for storing the products and cart items
@@ -17,11 +19,18 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(()=>{
+const storedCart=getShoppingCart();
+console.log(storedCart)
+  },([]))
+
   // Function to handle adding a product to the cart
   const handleAddToCart = (product) => {
     // Adds the selected product to the cart state
     const newcart = [...cart, product];
     setCart(newcart);
+    addToDb(product.id);
+
   };
 
   return (
